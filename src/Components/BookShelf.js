7220fbc529dfee48c0d9c44cb20e57bf9c48047d
Taina Conde <tainabatistaconde@gmail.com>
2020-12-15 17:06:38 -0300

    feat: BookShelf

---
+++ src/Components/BookShelf.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function BookShelf (props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default BookShelf;
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -5,11 +5,11 @@ class ListBooks extends React.Component {
         return (
             <div>
                 <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
+                    <div className="list-books-title">
+                        <h1>MyReads</h1>
+                    </div>
+                    <div className="list-books-content">
+                <div>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
