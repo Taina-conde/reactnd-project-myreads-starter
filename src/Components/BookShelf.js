b6e158cd5c28ce6b7551662f4e51eb2e02b6445a
Taina Conde <tainabatistaconde@gmail.com>
2020-12-15 19:17:49 -0300

    feat: BookShelf map()

--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Book from './Book';
 
 function BookShelf (props) {
     return (
         <div>
             <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
+                <h2 className="bookshelf-title">{props.bookshelfTitle}}</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      </>
+                      {props.books.map( bood => (
+                          <Book
+                            
+                          />
+                      ))}
                     </ol>
                   </div>
                 </div>
