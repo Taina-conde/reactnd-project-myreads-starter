b01d716d73126569bde9f8d00783f9e8a06c96b1
Taina Conde <tainabatistaconde@gmail.com>
2020-12-16 16:21:50 -0300

    feat: Search Link on close button

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Search extends React.Component {
     render() {
@@ -6,7 +7,10 @@ class Search extends React.Component {
             <div>
                 <div className="search-books">
                     <div className="search-books-bar">
-                        <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+                        <Link to = "/">
+                            <button className="close-search">Close</button>
+                        </Link>
+                        
                         <div className="search-books-input-wrapper">
                             {/*
                             NOTES: The search from BooksAPI is limited to a particular set of search terms.
