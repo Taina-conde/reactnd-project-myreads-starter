d4847a322fa204eb48edceea95273492ae50798e
Taina Conde <tainabatistaconde@gmail.com>
2020-12-16 18:07:09 -0300

    feat: Book cover image

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -9,7 +9,7 @@ class Book extends Component {
                 <li>
                     <div className="book">
                         <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: bookCover }}></div>
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookCover})` }}></div>
                             <div className="book-shelf-changer">
                               <select>
                                 <option value="move" disabled>Move to...</option>
