f4d0fbe97ab81e9395e2b5fc53fa89b8d4604a64
Taina Conde <tainabatistaconde@gmail.com>
2020-12-16 14:42:43 -0300

    feat: Link in OpenSearchButton

--- src/Components/OpenSearchButton.js
+++ src/Components/OpenSearchButton.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function OpenSearchButton(props) {
     return(
         <div className="open-search">
-            <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+            <Link to  = "/search">Add a book</Link>
         </div>
     )
 }
